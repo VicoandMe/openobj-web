77fc6433f4b93383968b814b60bb7bfb8f928e52
muzixiaoxin <45716108@qq.com>
2016-02-28 20:27:15 +0800

    create css

--- openobj/openobj/settings.py
+++ openobj/openobj/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    join(BASE_DIR, 'static')
+]
