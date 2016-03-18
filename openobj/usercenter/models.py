58a69cc991065b3e660a26609d46f61eeb5a4f43
tyrael <45716108@qq.com>
2016-03-18 18:35:29 +0800

    update

--- openobj/usercenter/models.py
+++ openobj/usercenter/models.py
@@ -6,12 +6,13 @@ from django.utils.timezone import now
 # Create your models here.
 
 
-class User(models.Model):
-    account = models.CharField(max_length=200, verbose_name=u'用户名')
-    password = models.CharField(max_length=200, verbose_name=u'密码')
-    nickname = models.CharField(max_length=50, verbose_name=u'昵称')
-    insert_time = models.DateTimeField(default=now, verbose_name=u'插入时间')
-    update_time = models.DateTimeField(verbose_name=u'更新时间', blank=True, null=True)
+class UserAccount(models.Model):
+    guid = models.AutoField(primary_key=True)
+    account = models.CharField(max_length=200, verbose_name='用户名')
+    password = models.CharField(max_length=200, verbose_name='密码')
+    nickname = models.CharField(max_length=50, verbose_name='昵称')
+    insert_time = models.DateTimeField(default=now, verbose_name='插入时间')
+    update_time = models.DateTimeField(verbose_name='更新时间', blank=True, null=True)
 
     def __unicode__(self):
         return self.account
