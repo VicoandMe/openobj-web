2227139c54f9026ba6a79e2b237a3905587ed958
muzixiaoxin <45716108@qq.com>
2016-03-24 17:21:44 +0800

    update classify

--- openobj/project/templatetags/project_filter.py
+++ openobj/project/templatetags/project_filter.py
@@ -1,8 +1,10 @@
-from django.templatetags.static import register
 from django import template
+register = template.Library()
 
 
 @register.filter(name='classify_spread')
-def classify_spread(classify_first_id):
-    return True
+def classify_spread(classify_first_id, show_id):
+    if classify_first_id == show_id:
+        return 'in'
+    return ''
 
