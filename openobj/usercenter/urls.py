8204f3a9de1d7d2079bec3db7098d40e22bd8bd1
Abel <abel.liu.china@outlook.com>
2016-04-07 16:30:47 +0800

    usercenter static page

--- openobj/usercenter/urls.py
+++ openobj/usercenter/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^register/success/$', views.register_success, name='register_success'),
     url(r'^logout/$', views.logout, name='logout'),
-    url(r'^email/verify/$', views.verify_email_code, name='verify_email_code')
+    url(r'^email/verify/$', views.verify_email_code, name='verify_email_code'),
+    url(r'^my/$', views.user_index, name='user_index'),
+    url(r'^info/$', views.user_info, name='user_info'),
+    url(r'^account/$', views.user_account, name='user_account'),
 ]
--- openobj/usercenter/views.py
+++ openobj/usercenter/views.py
@@ -64,5 +64,47 @@ def verify_email_code(request):
         code = request.POST.get("code")
         status, msg = logic.verify_register_email(code)
         return response_helper.http_response_json(status, msg, {})
+
+
+@ensure_csrf_cookie
+def user_index(request):
+    """
+    个人首页
+    """
+    if request.method == "POST":
+        pass
     else:
-        return render_to_response('usercenter/register_email_verify.html')
+        if not request.session.get('guid'):
+            return HttpResponseRedirect('/')
+        else:
+            return response_helper.render_response_html(request, 'usercenter/user_index.html', {})
+
+@ensure_csrf_cookie
+def user_info(request):
+    """
+    个人资料
+    """
+    if request.method == "POST":
+        pass
+    else:
+        if not request.session.get('guid'):
+            return HttpResponseRedirect('/')
+        else:
+            return response_helper.render_response_html(request, 'usercenter/user_info.html', {})
+
+
+@ensure_csrf_cookie
+def user_account(request):
+    """
+    账户设置
+    """
+    if request.method == "POST":
+        pass
+        # password = request.POST.get("password")
+        # status, msg = logic.login(request, email, password)
+        # return response_helper.http_response_json(status, msg, {})
+    else:
+        if not request.session.get('guid'):
+            return HttpResponseRedirect('/')
+        else:
+            return response_helper.render_response_html(request, 'usercenter/user_account.html', {})
