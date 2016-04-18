e97727c851eaac22748c6a83efc8f063a0cd6a9d
muzixiaoxin <45716108@qq.com>
2016-04-18 21:52:04 +0800

    update project

--- openobj/project/templatetags/project_filter.py
+++ openobj/project/templatetags/project_filter.py
@@ -1,4 +1,7 @@
 from django import template
+
+from project.models import Project
+
 register = template.Library()
 
 
@@ -8,3 +11,25 @@ def classify_spread(classify_first_id, show_id):
         return 'in'
     return ''
 
+
+@register.filter(name='difficulty')
+def difficulty(difficulty):
+    for diff in Project.DIFF_TYPE:
+       if diff[0] == difficulty:
+           return diff[1]
+    return difficulty
+
+
+@register.filter(name='code_pattern')
+def code_pattern(code_pattern):
+    for code in Project.CODE_PATTER:
+       if code[0] == code_pattern:
+           return code[1]
+    return code_pattern
+
+@register.filter(name='repository')
+def repository(repository):
+    for rep in Project.REPOSITORY:
+        if rep[0] == repository:
+            return rep[1]
+    return repository
